ebdc16fa3c6d5e99a3aada4494cec26046594f9f
Udinei <ukaliko@gmail.com>
2019-09-09 11:26:27 -0400

    Insercao de Usuario com POST

--- src/main/java/com/udineisilva/workshopmongo/resources/UserResource.java
+++ src/main/java/com/udineisilva/workshopmongo/resources/UserResource.java
@@ -1,26 +1,33 @@
 package com.udineisilva.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.udineisilva.workshopmongo.domain.User;
 import com.udineisilva.workshopmongo.dto.UserDTO;
 import com.udineisilva.workshopmongo.services.UserService;
 
+/**
+ * Cada metodo dessa classe é considerado um endpoint
+ * 
+ * */
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
 	@Autowired
 	private UserService service;
-	
+
 	
 	// "{/id}" - complemento de /users/id - indicando que 
 	// @PathVariable - indica que o {/id} sera o parametro do metodo e que vira na url
@@ -40,6 +47,21 @@ public class UserResource {
 		// collect(collectors.toList()) retorna os objetos em uma list
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+    
+	// @RequestBody - para que esse endpoint (metodo) aceite o DTO 
+	// O caminho sera o default /users
+	// @ResponseEntity<Void> - vai retornar um obj  vazio
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		
+		// colocar no cabecaho a localização url(endereco) do novo objeto criado 
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		// retorna o codigo 201(codigo http quando um novo recurso é criado) e localizacao do recurso criado
+		return ResponseEntity.created(uri).build();
 	}
 	
 	
