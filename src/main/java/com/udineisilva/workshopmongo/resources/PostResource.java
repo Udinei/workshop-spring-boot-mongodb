53a3450e5ebf942f9dd0305fd60feb7ef8fd1fbb
Udinei <ukaliko@gmail.com>
2019-09-10 11:09:01 -0400

    Consulta simples com query methods

--- src/main/java/com/udineisilva/workshopmongo/resources/PostResource.java
+++ src/main/java/com/udineisilva/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.udineisilva.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udineisilva.workshopmongo.domain.Post;
+import com.udineisilva.workshopmongo.resources.util.URL;
 import com.udineisilva.workshopmongo.services.PostService;
 
 /**
@@ -30,6 +34,13 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	
+	// @RequestParam(value="text", defaultValue="") - recebe o criterio de busca como parametro "?" (text é o nome do parametro)
+	// e nao como uma variavel de url que usa "/var" por exemplo: @PathVariable String id
+	@RequestMapping(value="/titlesearch",method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findBytitle(text);
+		return ResponseEntity.ok().body(list);
+	}	
 	
 }
